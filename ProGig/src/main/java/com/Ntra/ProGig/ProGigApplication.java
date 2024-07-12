fd8ea36fcf5e1038b76dd73b25b60a7e373d7786
harshil malaviya <harshilmalaviya@gmail.com>
2024-07-12 10:49:07 +0530

    just start the project

---
+++ ProGig/src/main/java/com/Ntra/ProGig/ProGigApplication.java
@@ -0,0 +1,13 @@
+package com.Ntra.ProGig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProGigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProGigApplication.class, args);
+	}
+
+}
---
+++ ProGig/src/test/java/com/Ntra/ProGig/ProGigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ntra.ProGig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProGigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
