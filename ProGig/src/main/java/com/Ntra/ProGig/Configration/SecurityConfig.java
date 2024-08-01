9b9d60abe70d353f61078b9066861f15a009ce12
Dhyey <dhyeybaldha90@gmail.com>
2024-08-01 11:04:32 +0530

    Proposal Section is completed

--- ProGig/src/main/java/com/Ntra/ProGig/Configration/SecurityConfig.java
+++ ProGig/src/main/java/com/Ntra/ProGig/Configration/SecurityConfig.java
@@ -4,28 +4,22 @@ package com.Ntra.ProGig.Configration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecutiryConfig {
+public class SecurityConfig {
      @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         Req->Req.requestMatchers("/freelancer/**")
                                 .permitAll()
-                                .requestMatchers("/clients/**","/Transaction/**")
+                                .requestMatchers("/clients/**","/Transaction/**","/proposals/**")
                                 .permitAll()
                 )
                 .build();
