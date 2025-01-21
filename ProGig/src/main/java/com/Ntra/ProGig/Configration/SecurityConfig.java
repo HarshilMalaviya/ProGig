e4a8312721cc4fe679aaec052b2e5a0020252437
Dhyey <dhyeybaldha90@gmail.com>
2025-01-21 10:53:00 +0530

    Docker implementation changes

--- ProGig/src/main/java/com/Ntra/ProGig/Configration/SecurityConfig.java
+++ ProGig/src/main/java/com/Ntra/ProGig/Configration/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.Ntra.ProGig.Configration;
 
 import com.Ntra.ProGig.Filter.JwtAuthFilter;
 import com.Ntra.ProGig.Service.UserServiceImpl;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- ProGig/src/main/java/com/Ntra/ProGig/Repository/StakHolderRepo.java
+++ ProGig/src/main/java/com/Ntra/ProGig/Repository/StakHolderRepo.java
@@ -2,9 +2,10 @@ package com.Ntra.ProGig.Repository;
 
 import com.Ntra.ProGig.Entity.StakHolder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface StakHolderRepo extends JpaRepository<StakHolder,Integer> {
     @Override
     Optional<StakHolder> findById(Integer Id);
